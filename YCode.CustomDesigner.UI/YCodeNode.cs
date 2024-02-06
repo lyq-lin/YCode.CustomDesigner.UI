80e97d07551dac83b0f2e146a9ef87c7f6bec74c
YCode <joestudy@petalmail.com>
2024-02-06 16:28:49 +0800

    feat: 新增YCodeNode

---
+++ YCode.CustomDesigner.UI/YCodeNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace YCode.CustomDesigner.UI
+{
+	public class YCodeNode : ContentControl
+	{
+		static YCodeNode()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(
+				typeof(YCodeNode),
+				new FrameworkPropertyMetadata(typeof(YCodeNode)));
+		}
+
+		internal Point Left { get; private set; }
+		internal Point Right { get; private set; }
+	}
+}
