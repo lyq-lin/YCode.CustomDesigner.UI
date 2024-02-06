6183f4c59d0a37234574a154ce63b9e1d7727b50
YCode <joestudy@petalmail.com>
2024-02-06 17:15:24 +0800

    feat: 新增拓展方法YCodeExtension

---
+++ YCode.CustomDesigner.UI/YCodeExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace YCode.CustomDesigner.UI
+{
+	internal static class YCodeExtension
+	{
+		internal static T? FindParent<T>(this UIElement element) where T : UIElement
+		{
+			if (element is T value)
+			{
+				return value;
+			}
+			else
+			{
+				var parent = VisualTreeHelper.GetParent(element);
+
+				if (parent is UIElement last)
+				{
+					return last.FindParent<T>();
+				}
+			}
+
+			return default;
+		}
+	}
+}
