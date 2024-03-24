b9c16ac25b21a2e193ebfdb58cecbcb3cdbb010f
YCode <joestudy@petalmail.com>
2024-03-24 20:31:07 +0800

    feat: Canvas右键菜单

--- YCode.CustomDesigner.UI/YCodeCanvas.cs
+++ YCode.CustomDesigner.UI/YCodeCanvas.cs
@@ -9,10 +9,28 @@ namespace YCode.CustomDesigner.UI
 	{
 		private Point? _elementPoint;
 
+		public YCodeCanvas()
+		{
+			this.OnLoadMenu();
+		}
+
 		internal UIElement? CurrentElement { get; set; }
 
 		public event EventHandler<YCodeNodeDeletedEventArgs>? ElementDeleted;
 		public event EventHandler<YCodeNodeDeletingEventArgs>? ElementDeleting;
+
+		#region Dependency Property
+
+		public ICommand MenuDeletedCommand
+		{
+			get { return (ICommand)GetValue(MenuDeletedCommandProperty); }
+			set { SetValue(MenuDeletedCommandProperty, value); }
+		}
+
+		public static readonly DependencyProperty MenuDeletedCommandProperty =
+			DependencyProperty.Register("MenuDeletedCommand", typeof(ICommand), typeof(YCodeCanvas));
+
+		#endregion
 
 		protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
 		{
@@ -68,6 +86,23 @@ namespace YCode.CustomDesigner.UI
 					this.ElementDeleted?.Invoke(this, new YCodeNodeDeletedEventArgs(removeItem));
 				}
 			}
+		}
+
+		internal void OnLoadMenu()
+		{
+			this.ContextMenu = new ContextMenu();
+
+			var menuItem1 = new MenuItem
+			{
+				Header = "选项1"
+			};
+			this.ContextMenu.Items.Add(menuItem1);
+
+			var menuItem2 = new MenuItem
+			{
+				Header = "选项2"
+			};
+			this.ContextMenu.Items.Add(menuItem2);
 		}
 	}
 }
