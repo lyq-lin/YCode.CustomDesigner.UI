f4fd59bd61a524a9523540f2ed6a82667bb995eb
YCode <joestudy@petalmail.com>
2024-10-27 01:21:21 +0800

    refactor: 重构YCode.Ui

---
+++ YCode.CustomDesigner.UI/YCodeDesigner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace YCode.CustomDesigner.UI;
+
+public class YCodeDesigner : MultiSelector
+{
+    static YCodeDesigner()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(YCodeDesigner),
+            new FrameworkPropertyMetadata(typeof(YCodeDesigner))
+        );
+
+        FocusableProperty.OverrideMetadata(typeof(YCodeDesigner), new FrameworkPropertyMetadata(true));
+    }
+
+    private readonly TranslateTransform _translateTransform = new();
+    private readonly ScaleTransform _scaleTransform = new();
+
+    public YCodeDesigner()
+    {
+        var transform = new TransformGroup();
+        transform.Children.Add(_scaleTransform);
+        transform.Children.Add(_translateTransform);
+
+        this.SetValue(ViewportTransformPropertyKey, transform);
+    }
+
+    protected internal Panel ItemsHost { get; set; } = default!;
+
+    #region Dependency Properties
+
+    protected internal static readonly DependencyPropertyKey ViewportTransformPropertyKey =
+        DependencyProperty.RegisterReadOnly(
+            nameof(ViewportTransform),
+            typeof(Transform),
+            typeof(YCodeDesigner),
+            new FrameworkPropertyMetadata(new TransformGroup())
+        );
+
+    public static readonly DependencyProperty ViewportTransformProperty =
+        ViewportTransformPropertyKey.DependencyProperty;
+
+    public static readonly DependencyProperty ItemsExtentProperty = DependencyProperty.Register(
+        nameof(ItemsExtent), typeof(Rect), typeof(YCodeDesigner), new PropertyMetadata(default(Rect)));
+
+    public Rect ItemsExtent
+    {
+        get => (Rect)GetValue(ItemsExtentProperty);
+        set => SetValue(ItemsExtentProperty, value);
+    }
+
+    public Transform ViewportTransform => (Transform)GetValue(ViewportTransformProperty);
+
+    #endregion
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        this.ItemsHost = this.GetTemplateChild("PART_ItemsHost") as Panel ??
+                         throw new InvalidOperationException("PART_ItemsHost is missing or is not of type Panel.");
+    }
+}
