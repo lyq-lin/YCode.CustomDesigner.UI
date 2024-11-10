2da7f4cfa52ead23facf9be370f38a6f7dd43a14
YCode <joestudy@petalmail.com>
2024-11-10 10:01:36 +0800

    feat: FluxoConnector

---
+++ YCode.Designer.Fluxo/Nodes/Control/Connector/FluxoConnector.cs
@@ -0,0 +1,12 @@
+namespace YCode.Designer.Fluxo;
+
+internal class FluxoConnector : Control
+{
+    static FluxoConnector()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(FluxoConnector),
+            new FrameworkPropertyMetadata(typeof(FluxoConnector))
+        );
+    }
+}
